1dd40f1cd9cc436a655ba4729a684d20b930fcc1
NusratNadira <nusratnadira2@gmail.com>
2023-10-27 19:44:36 +0600

    update

--- JS/script.js
+++ JS/script.js
@@ -16,25 +16,3 @@ window.onscroll = () =>{
   navbar.classList.remove('active');
   loginForm.classList.remove('active');
 }
-
-var swiper = new Swiper(".review-slider",{
-  spaceBetween: 20,
-  centeredSlides: true,
-  grabCursor: true,
-  autoplay:{
-    delay: 2000,
-    disableOnInteraction: false,
-  },
-  loop: true,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
-});
